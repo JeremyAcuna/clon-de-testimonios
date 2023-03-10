7a18eb71c1055fc9ab4911469b3255431165ed16
JeremyAcuna <jeremydannyacunasiqueiros5b@gmail.com>
2023-03-09 20:44:26 -0500

    first commit

---
+++ src/componentes/Testimonio.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../StyleSheet/Testimonio.css';
+
+function Testimonio(props){
+    return(
+        <div className='contenedor-testimonio'>
+          <img
+            className='imagen-testimonio'
+            src={require(`../Imagenes/${props.imagen}.png`)}
+            alt= {`Imagen de ${props.nombre}`} />
+          <div className='contenedor-texto-testimonio'>
+            <p className='nombre-testimonio'>
+              <strong>{props.nombre}</strong> en <strong>{props.pais}</strong>
+            </p>
+            <p className='cargo-testimonio'>
+              {props.cargo} en <strong>{props.empresa}</strong>
+            </p>
+            <p className='texto-testimonio'>"{props.testimonio}"</p>
+          </div>
+        </div>
+    );
+}  
+
+export default Testimonio;
